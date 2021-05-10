6faef93bf81fe088b298b227bc7a9f58ae918aa4
Armine-web <83885299+Armine-web@users.noreply.github.com>
2021-05-10 17:21:06 +0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Oll from "./Oll";
+
+function App() {
+  return (
+    <div className="App">
+
+    <Oll
+    text ="WEATHER"
+    /> 
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ Oll.js
@@ -0,0 +1,13 @@
+
+
+function Oll(props) {
+    const text = props.text;
+return (
+    <span>
+       
+       <a href= "https://goweather.herokuapp.com/weather/" > {text}</a>
+    </span>
+)
+}
+
+export default Oll;
